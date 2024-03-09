10abf3b10b0d7bf357bf6b847f6197a4f2a124e1
Sahin Dursun <sahin.dursun@relexsolutions.com>
2024-03-09 12:27:20 +0100

    weird diff bug

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -8,20 +8,19 @@ import (
 
 // provides all functions to execute db queries and transactions
 type Store struct {
-	*Queries 
+	*Queries
 	db *sql.DB
-
 }
 
 func NewStore(db *sql.DB) *Store {
 	return &Store{
-		db: db,
+		db:      db,
 		Queries: New(db),
 	}
 }
 
 func (s *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
-	tx, err := s.db.BeginTx(ctx, nil) 
+	tx, err := s.db.BeginTx(ctx, nil)
 
 	if err != nil {
 		return err
@@ -29,7 +28,7 @@ func (s *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
 
 	q := New(tx)
 
-	err = fn(q) 
+	err = fn(q)
 	if err != nil {
 		if rbErr := tx.Rollback(); rbErr != nil {
 			return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
@@ -40,33 +39,35 @@ func (s *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
 	return tx.Commit()
 
 }
+
 // TransferTXParams contains the input parameters of the transaction
 type TransferTxParams struct {
 	FromAccountID int64 `json:"from_account_id"`
-	ToAccountID int64 `json:"to_account_id"`
-	Amount int64 `json:"amount"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
 }
+
 // TransferTxResult contains the result of the transfer transaction
 type TransferTxResult struct {
-	Transfer Transfer `json:"transfer"`
-	FromAccount Account `json:"account"`
-	ToAccount Account `json:"account"`
-	FromEntry Entry `json:"entry"`
-	ToEntry Entry `json:"entry"`
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"account"`
+	ToAccount   Account  `json:"account"`
+	FromEntry   Entry    `json:"entry"`
+	ToEntry     Entry    `json:"entry"`
 }
 
 // performs money transfer between two accounts
-// it creates a transfer record, adds account entries, and updates accounts' balance 
+// it creates a transfer record, adds account entries, and updates accounts' balance
 // within a single database transaction
 func (s *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
 	var result TransferTxResult
 
-	err := s.execTx(ctx, func (q *Queries) error {
+	err := s.execTx(ctx, func(q *Queries) error {
 		var err error
 		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
 			FromAccountID: arg.FromAccountID,
-			ToAccountID: arg.ToAccountID,
-			Amount: arg.Amount,
+			ToAccountID:   arg.ToAccountID,
+			Amount:        arg.Amount,
 		})
 
 		if err != nil {
@@ -75,7 +76,7 @@ func (s *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferT
 
 		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
 			AccountID: arg.FromAccountID,
-			Amount: -arg.Amount,
+			Amount:    -arg.Amount,
 		})
 
 		if err != nil {
@@ -84,7 +85,7 @@ func (s *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferT
 
 		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
 			AccountID: arg.ToAccountID,
-			Amount: arg.Amount,
+			Amount:    arg.Amount,
 		})
 
 		if err != nil {
@@ -92,7 +93,32 @@ func (s *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferT
 		}
 
 		//TODO: update accounts' balances. Complex because we need to prevent locking.
+		// get account -> update its balance. often done wrong without locking mechanism
+		account1, err := q.GetAccountForUpdate(ctx, arg.FromAccountID)
+		if err != nil {
+			return err
+		}
+		result.FromAccount, err = q.UpdateAccount(ctx, UpdateAccountParams{
+			ID:      arg.FromAccountID,
+			Balance: account1.Balance - arg.Amount,
+		})
 
+		if err != nil {
+			return err
+		}
+
+		account2, err := q.GetAccountForUpdate(ctx, arg.FromAccountID)
+		if err != nil {
+			return err
+		}
+		result.ToAccount, err = q.UpdateAccount(ctx, UpdateAccountParams{
+			ID:      arg.ToAccountID,
+			Balance: account2.Balance + arg.Amount,
+		})
+
+		if err != nil {
+			return err
+		}
 
 		return nil
 	})
