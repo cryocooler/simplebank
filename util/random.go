752f07fb901c78a36fe6bbe33044fe38e5fd7da0
Sahin Dursun <sahin.dursun@relexsolutions.com>
2024-03-17 18:26:37 +0100

    add users table with unique constraints

--- util/random.go
+++ util/random.go
@@ -7,16 +7,17 @@ import (
 )
 
 const alphabet = "abcdefghijklmnopqrstuvxyz"
+
 func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
 func RandomInt(min, max int64) int64 {
-	return min + rand.Int63n(max - min + 1) // min -> max-min 
+	return min + rand.Int63n(max-min+1) // min -> max-min
 }
 
 func RandomString(n int) string {
-	var sb strings.Builder 
+	var sb strings.Builder
 	k := len(alphabet)
 
 	for i := 0; i < n; i++ {
@@ -37,7 +38,7 @@ func RandomMoney() int64 {
 }
 
 func RandomCurrency() string {
-	currencies := []string{"EUR", "USD", "CAD", "CHF"}
+	currencies := []string{"EUR", "USD", "SEK", "CHF"}
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
 }
